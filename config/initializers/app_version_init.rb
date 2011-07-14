4dacba2cf36553a6ca7483ab87240a592e6687cd
Michael Stahnke <stahnma@puppetlabs.com>
2011-07-14 14:24:03 -0700

    (#6840) Remove need for VERSION file in puppet-dashboard
    Instead of always relying on the VERSION file being present, we now
    first test to see if it is, then fall back to using the output of
    git-describe to determine which version of the Dashboard we are running.
    
    Also, instead of just linking to the puppet-dashboard project on GitHub,
    we now link to the CHANGELOG file for the version we are running.
    
    Signed-off-by: Michael Stahnke <stahnma@puppetlabs.com>

--- config/initializers/app_version_init.rb
+++ config/initializers/app_version_init.rb
@@ -1,3 +1,23 @@
 # Get the Dashboard version number.
 
-APP_VERSION = File.read(Rails.root.join('VERSION')).strip.sub(/^v/, '')
+
+
+# Any officially released package or tarball of dashboard
+# will have a VERSION file that is generated at tarball
+# build time. 
+#
+# This allows for the APP_VERSION to be determined by git, so
+# developers can run out of master after a git checkout.
+def get_app_version
+  if File.exists?(Rails.root.join('VERSION'))
+    File.read(Rails.root.join('VERSION')).strip
+  else
+    if File.directory?(Rails.root.join('.git'))
+      `cd #{Rails.root}; git describe`.strip! rescue 'unknown'
+    else
+      'unknown'
+    end
+  end 
+end
+
+APP_VERSION = get_app_version
